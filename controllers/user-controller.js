92e22f9da1669a53baa2485e53666dbca2be5ceb
jolly01008 <jollgmll292@gmail.com>
2024-01-02 23:45:58 +0800

    feat:install mysql package & sequelize init & add User model

---
+++ controllers/user-controller.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
+const userController = {
+  signUp: (req, res, next) => {
+  }
+}
+
+module.exports = userController
---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    phone: DataTypes.STRING,
+    country: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
