826eb37d2def3ee693807cfac7188de0feda937f
jolly01008 <jollgmll292@gmail.com>
2024-01-04 23:55:37 +0800

    feat:add user signup api & error handler api

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,33 @@
 const bcrypt = require('bcryptjs')
-const User = require('../models/user')
+const { User } = require('../models')
 
 const userController = {
-  signUp: (req, res, next) => {
+  signUp: async (req, res, next) => {
+    try {
+      const { name, email, password, confirmPassword } = req.body
+      if (!name || !email || !password || !confirmPassword) throw new Error('每個欄位都需要填寫')
+      if (password !== confirmPassword) throw new Error('密碼與確認密碼不相符')
+
+      const userEmail = await User.findOne({ where: { email } })
+      if (userEmail) throw new Error('這個email已經註冊過了!')
+
+      const hash = await bcrypt.hash(password, 10)
+      const newUser = await User.create({
+        name,
+        email,
+        password: hash,
+        role: 'user',
+        avatar: 'https://imgur.com/a/RJmY7yQ'
+      })
+      const userData = newUser.toJSON()
+      delete userData.password
+      return res.json({
+        status: 'success',
+        data: { user: userData }
+      })
+    } catch (err) {
+      return next(err)
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const hostel = require('./modules/hostel')
+const userController = require('../controllers/user-controller')
+const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.use('/hostels', hostel)
+
+router.post('/signUp', userController.signUp)
+router.use('/', apiErrorHandler)
 
 module.exports = router
