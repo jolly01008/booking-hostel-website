3fb486f565fda346cfd42af9b23265673710a579
jolly01008 <jollgmll292@gmail.com>
2024-02-14 19:26:58 +0800

    feat:setting model associate

--- models/bed.js
+++ models/bed.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Bed extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
-      // define association here
+      Bed.belongsTo(models.Room, { foreignKey: 'roomId' })
+      Bed.hasMany(models.bookedBed, { foreignKey: 'bedId' })
     }
   };
   Bed.init({
--- models/landlord.js
+++ models/landlord.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Landlord extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
-      // define association here
+      Landlord.belongsTo(models.User, { foreignKey: 'userId' })
+      Landlord.hasMany(models.Hostel, { foreignKey: 'landlordId' })
     }
   };
   Landlord.init({
